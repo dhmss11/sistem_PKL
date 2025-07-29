a778c0dc9a615b3eb5383f8e2fa853b21b64371c
reando2 <halogaes113@gmail.com>
2025-07-29 15:07:28 +0700

    fix error route

--- monitor-suhu/app/api/golonganstock/keterangan/[keterangan]/route.js
+++ monitor-suhu/app/api/golonganstock/keterangan/[keterangan]/route.js
@@ -3,16 +3,18 @@ import { API_ENDPOINTS } from '@/app/api/api';
 import { NextResponse } from 'next/server';
 
 export async function GET(_, { params }) {
-  const { keterangan } = params;
+  // ✅ AWAIT params dulu sebelum destructuring
+  const { keterangan } = await params;
 
   try {
     const response = await Axios.get(API_ENDPOINTS.GET_ALL_KETERANGAN_STOCK(keterangan));
-      return NextResponse.json({
-        status: '00',
-        message: 'Berhasil mengambil data golongan stock berdasarkan keterangan',
-        data: Array.isArray(response.data) ? response.data : response.data?.data || [],
-      });
-
+    
+    return NextResponse.json({
+      status: '00',
+      message: 'Berhasil mengambil data golongan stock berdasarkan keterangan',
+      data: Array.isArray(response.data) ? response.data : response.data?.data || [],
+    });
+   
   } catch (error) {
     return NextResponse.json(
       {
